a7a4f282999e729d3e5a33d7182358d8ce78b141
Jason Zhao <silverzhaojr@gmail.com>
2012-07-20 23:44:41 +0800

    增加自动获取网关功能，增加支持64位系统

--- install_bras.sh
+++ install_bras.sh
@@ -13,21 +13,33 @@ if [ $UID -ne 0 ]; then
     exit 1
 fi
 
-if uname -a | grep "ARCH" &> /dev/null; then
+if grep "Arch" /etc/issue &> /dev/null; then
     XL2TPD_PATH='\/etc\/rc.d'
-    XL2TPD_PACKAGE=xl2tpd-1.3.0-1-i686.pkg.tar.xz
     INSTALL_CMD="pacman -U"
     NEED_INSTALL=1
-elif uname -a | grep "Ubuntu" &> /dev/null; then
+    if [ $(uname -m) = "i686" ]; then
+        XL2TPD_PACKAGE=xl2tpd-1.3.0-1-i686.pkg.tar.xz
+    else
+        XL2TPD_PACKAGE=xl2tpd-1.3.0-1-x86_64.pkg.tar.xz
+    fi
+elif grep "Ubuntu" /etc/issue &> /dev/null; then
     XL2TPD_PATH='\/etc\/init.d'
-    XL2TPD_PACKAGE=xl2tpd_1.2.7_dfsg-1_i386.deb
     INSTALL_CMD="dpkg -i"
     NEED_INSTALL=1
-elif uname -a | grep "fc" &> /dev/null; then
+    if [ $(uname -m) = "i686" ]; then
+        XL2TPD_PACKAGE=xl2tpd_1.2.7_dfsg-1_i386.deb
+    else
+        XL2TPD_PACKAGE=xl2tpd_1.2.7_dfsg-1_amd64.deb
+    fi
+elif grep "Fedora" /etc/issue &> /dev/null; then
     XL2TPD_PATH='\/etc\/rc.d\/init.d'
-    XL2TPD_PACKAGE=xl2tpd-1.3.0-1.fc16.i686.rpm
     INSTALL_CMD="yum localinstall"
     NEED_INSTALL=1
+    if [ $(uname -m) = "i686" ]; then
+        XL2TPD_PACKAGE=xl2tpd-1.3.0-1.fc16.i686.rpm
+    else
+        XL2TPD_PACKAGE=xl2tpd-1.3.0-1.fc16.x86_64.rpm
+    fi
 else
     NEEED_INSTALL=0
     echo "Are you using Archlinux/Ubuntu/Fedora? \
@@ -42,8 +54,6 @@ while [ -z "$BRAS_PASSWORD" ]; do
     read -s -p 'BRAS_PASSWORD: (Input is hidden.)' BRAS_PASSWORD
 done
 echo ""
-
-GATEWAY=$(ip route | grep 'default' | awk '{print $3}')
 
 if ! which xl2tpd &> /dev/null && [ $NEED_INSTALL = "1" ]; then
     wget --no-proxy http://bbs.nju.edu.cn/file/S/silverzhao/$XL2TPD_PACKAGE
@@ -77,14 +87,21 @@ EEOOFF
 
 mkdir -p /usr/local/sbin
 
-sed -e "s/172.25.49.1/$GATEWAY/" -e "s/XL2TPD_PATH/$XL2TPD_PATH/" \
+sed -e "s/XL2TPD_PATH/$XL2TPD_PATH/" \
 > /usr/local/sbin/bras-ctrl << "EEOOFF"
 #!/bin/bash
 
 case $1 in
 route)
-GATEWAY=172.25.49.1
 {
+    if [ "$2" = "add" ]; then
+        GATEWAY=$(ip route | grep 'default' | awk '{print $3}')
+        ip route replace default dev ppp0
+    elif [ "$2" = "del" ]; then
+        GATEWAY=$(ip route | grep '219.219.112.0' | awk '{print $3}')
+        ip route replace default via $GATEWAY
+    fi
+
     ip route $2 58.192.32.0/20 via $GATEWAY
     ip route $2 58.192.48.0/21 via $GATEWAY
     ip route $2 114.212.0.0/16 via $GATEWAY
@@ -97,12 +114,6 @@ GATEWAY=172.25.49.1
     ip route $2 210.28.128.0/20 via $GATEWAY
     ip route $2 210.29.240.0/20 via $GATEWAY
     ip route $2 219.219.112.0/20 via $GATEWAY
-
-    if [ "$2" = "add" ]; then
-	ip route replace default dev ppp0
-    elif [ "$2" = "del" ]; then
-	ip route replace default via $GATEWAY
-    fi
 } &> /dev/null
 ;;
 
